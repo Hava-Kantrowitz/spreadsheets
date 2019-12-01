8ef4b2a22763f66452d8a67632fe65d0688a4f5c
Hava Kantrowitz <havakhp@gmail.com>
2019-12-01 17:35:43 -0500

    uh this is wack

--- src/edu/cs3500/spreadsheets/ProviderRunner.java
+++ src/edu/cs3500/spreadsheets/ProviderRunner.java
@@ -1,15 +1,17 @@
+package edu.cs3500.spreadsheets;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 import edu.cs3500.spreadsheets.model.BasicSpreadsheet;
+import edu.cs3500.spreadsheets.provider.view.IView;
+import edu.cs3500.spreadsheets.provider.view.JPanelGrid;
 import edu.cs3500.spreadsheets.view.SpreadsheetEditableView;
-import edu.cs3500.spreadsheets.view.SpreadsheetView;
 
 /**
- * Runner for the gui view, used solely for testing purposes.
+ * Runner for the provider's gui view. Used solely for testing purposes.
  */
-public class SpreadsheetRunner {
-
+public class ProviderRunner {
   /**
    * Entry into running the gui view. This main is solely for testing purposes.
    * @param args command line arguments to the program
@@ -18,9 +20,7 @@ public class SpreadsheetRunner {
   public static void main(String[] args) throws FileNotFoundException {
     BasicSpreadsheet sheet = new BasicSpreadsheet();
     sheet.initializeSpreadsheet(new FileReader("C:\\Users\\havak\\IdeaProjects\\nextTry\\src\\edu\\cs3500\\spreadsheets\\testFiles\\testingSpecial"));
-    SpreadsheetView view = new SpreadsheetEditableView(sheet);
-    view.render();
+    //IView view = new JPanelGrid(sheet);
+    //view.render();
   }
-
-
 }
---
+++ src/edu/cs3500/spreadsheets/provider/view/IView.java
@@ -0,0 +1,40 @@
+package edu.cs3500.spreadsheets.provider.view;
+
+import java.awt.event.ActionListener;
+
+import edu.cs3500.spreadsheets.model.Coord;
+import edu.cs3500.spreadsheets.provider.model.IViewWorksheet;
+
+public interface IView {
+
+  /**
+   * SetLabel modifies the label of a JFRAME.
+   *
+   * @param s The new string to become the label.
+   */
+  void setLabel(String s);
+
+  /**
+   * Initializes the view with a model and window dimensions
+   *
+   * @param model  The WorksheetModel storing the cell information.
+   * @param width  The width of the View window.
+   * @param height The height of the View window.
+   */
+  public void initialize(IViewWorksheet model, int width, int height);
+
+  /**
+   * Displays the view.
+   */
+  void display();
+
+  /**
+   * Adds an actionListener to this JPanel.
+   * @param mouseListener       the actionListener to be added.
+   */
+  void addActionListener(ActionListener mouseListener);
+
+  JPanelCell getCell(Coord c);
+
+  void setToolbar(String s);
+}
